78047344a2c452488aa5e979e6cc4287ce7d8388
sloorush <aarush.bhatt@gmail.com>
2022-04-04 11:27:47 +0530

    add: basic notetaker application using spock

---
+++ README.md
@@ -0,0 +1,7 @@
+# notetaker-hs
+
+Simple and sample notetaker application used to take notes written in Haskell.
+
+This small project was made while learing Haskell.
+
+Uses [Spock](https://hackage.haskell.org/package/Spock) and its built-in support for server state. Server state is used to implement an in-memory database of notes.
--- app/Main.hs
+++ app/Main.hs
@@ -2,22 +2,60 @@
 
 module Main where
 
+import Control.Monad (forM_)
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.IORef
+import Data.Semigroup ((<>))
+import Data.Text (Text)
 import Lucid
 import Web.Spock
 import Web.Spock.Config
-import Web.Spock.Lucid
+import Web.Spock.Lucid (lucid)
 
--- newtype ServerState = ServerState {notes :: IORef [Note]}
+data Note = Note {author :: Text, contents :: Text}
 
-type Server a = SpockM () () () a
+newtype ServerState = ServerState {notes :: IORef [Note]}
+
+type Server a = SpockM () () ServerState a
 
 app :: Server ()
-app = get root $
-  lucid $ do
-    h1_ "suppp"
-    p_ "yooo"
+app = do
+  get root $ do
+    notes' <- getState >>= (liftIO . readIORef . notes)
+    lucid $ do
+      h1_ "Notes"
+      p_ "Yooo! How you doing?"
+      ul_ $
+        forM_ notes' $ \note -> li_ $ do
+          toHtml (author note)
+          ": "
+          toHtml (contents note)
+      h2_ "New Note"
+      form_ [method_ "post"] $ do
+        label_ $ do
+          "Author: "
+          input_ [name_ "author"]
+        label_ $ do
+          "Contents: "
+          textarea_ [name_ "contents"] ""
+        input_ [type_ "submit", value_ "Add Note"]
+
+  post root $ do
+    author <- param' "author"
+    contents <- param' "contents"
+    notesRef <- notes <$> getState
+    liftIO $
+      atomicModifyIORef' notesRef $ \notes ->
+        (notes <> [Note author contents], ())
+    redirect "/"
 
 main :: IO ()
 main = do
-  cfg <- defaultSpockCfg () PCNoDatabase ()
+  st <-
+    ServerState
+      <$> newIORef
+        [ Note "Rush" "Haskell is nice :')",
+          Note "sloorush" "Spock? Is this star trek?"
+        ]
+  cfg <- defaultSpockCfg () PCNoDatabase st
   runSpock 8080 (spock cfg app)
